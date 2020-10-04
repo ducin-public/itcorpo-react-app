ab4ab0b4a239c07f6c080e2ebeeb0f4b1c29a3b2
Tomasz Ducin <tomasz@ducin.it>
2020-10-04 20:51:59 +0200

    update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Emplojisy!
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Emplojisy!
+      </header>
+    </div>
+  );
 }
+
 
 export default App;
